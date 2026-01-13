3bbccd974c11aa2b68e416b3c89bdfb16bcad9e1
Sachin Kumar <sachin8935@Sachins-MacBook-Air.local>
2026-01-13 16:27:00 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ const corsOptions = {
       'http://localhost:3000',
       'http://127.0.0.1:5500',
       'http://localhost:5500',
+      'https://todo-tau-bay.vercel.app',
       process.env.CLIENT_URL,
       process.env.VERCEL_URL,
       // Add common patterns for Vercel and Netlify
